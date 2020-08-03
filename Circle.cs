893a1d1050dbd40a48932e180f7a5111afd01deb
ryan <rynmcevoy21@gmail.com>
2020-08-02 22:17:15 -0400

    First Commit

---
+++ Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10
+{
+    public class Circle
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double Radius { get; set; }
+        public double CalculateCircumference
+        {
+            get { return 2 * Math.PI * Radius; }
+        }
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference);
+        }
+        public double CalculateArea
+        {
+           get { return Radius * Radius * Math.PI; }
+        }
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea);
+        }
+        private string FormatNumber(double r)
+        {
+            return Math.Round(r, 2).ToString();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10
+{
+    public class Program
+    {
+        public static bool UserContinueSelection { get; private set; } = true;
+
+        static void Main(string[] args)
+        {
+            while (UserContinueSelection == true)
+            {
+
+                List<Circle> circleList = new List<Circle>();
+                Console.WriteLine("Enter a radius");
+                var userInput = Console.ReadLine();
+                bool success = Double.TryParse(userInput, out double radius);
+
+                if (success)
+                {
+                    var circle = new Circle(radius);
+                    circleList.Add(circle);
+
+                    Console.WriteLine($"The circumfrance is: {circle.CalculateFormattedCircumference()}");
+                    Console.WriteLine($"The area is: {circle.CalculateFormattedArea()}");
+
+                    UserContinue();
+                }
+                else
+                {
+                    Console.WriteLine($"Plese put valid input");
+                }
+            }
+        }
+        private static void UserContinue()
+        {
+            Console.WriteLine("Do you want to continue? (Y/N)");
+            var userContinue = Console.ReadLine();
+
+            if (userContinue.Contains("Y", StringComparison.InvariantCultureIgnoreCase))
+            {
+                UserContinueSelection = true;
+            }
+            else
+            {
+                UserContinueSelection = false;
+            }
+        }
+    }
+}
